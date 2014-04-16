6144781e38f7aeacd86f4e221d4140e6ab4be523
Newlin Wilkins <nwilkins89@gmail.com>
2014-04-15 21:54:25 -0500

    class planning exercise

---
+++ oop_blackjack.rb
@@ -0,0 +1,19 @@
+#This is the object oriented version of the blackjack game
+game # that can be player
+  That manages turns of player and dealer
+  Calculates scores of palyer and dealer
+    determines whether aces are 11 or 1
+  Determines who has won or lost
+player # by player
+  that hits or stays
+dealer # and dealer
+  that hits until <= 17
+deck # with a deck
+  that has 11 values across 4 suits
+  can be shuffled
+  (can have more than 1 deck)
+  gives cards to player and dealer, and removes from deck those cards
+cards # of cards
+  have a suit and a value
+
+
